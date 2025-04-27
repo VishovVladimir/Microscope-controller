19f9f5058e274b1041fd1b579a301b8757424d33
v.vishov <v.vishov@unlimit.com>
2025-04-27 12:59:53 +0200

    recording

--- web_server/video_streamer.py
+++ web_server/video_streamer.py
@@ -1,8 +1,11 @@
 import re
 import subprocess
 import logging
-from time import sleep
+from time import sleep, time, strftime, localtime
 import threading
+import os
+import cv2
+import numpy as np
 
 
 class VideoStreamer:
@@ -19,6 +22,9 @@ class VideoStreamer:
     # Get frames in continuously mode (for threading)
     START_JPEG_IMG_FRAME_CONTINUOUSLY_SCRIPT_FILE_PATH="/home/pi/.microscope/web_server/stream_scripts/camera_capture_frames_continuously.sh"
 
+    SAVE_FOLDER = os.path.expanduser("~/saved-video/")
+    LOG_FILE_PATH = os.path.join(SAVE_FOLDER, "video_log.txt")
+
     is_stream_started_flag = False
     is_stop_pending_flag = False
     is_stop_done_flag = False
@@ -32,9 +38,17 @@ class VideoStreamer:
     # Lock to access self.last_captured_frame
     mutex = threading.Lock()
 
+    video_writer = None
+    recording_start_time = None
+    current_video_filename = None
+
+    is_recording = False
 
     def __init__(self):
         subprocess.call(self.STOP_STREAM_SCRIPT_FILE_PATH)
+        os.makedirs(self.SAVE_FOLDER, exist_ok=True)
+        if not os.path.exists(self.LOG_FILE_PATH):
+            open(self.LOG_FILE_PATH, 'w').close()
         #subprocess.call(self.SET_RES_1920X1080_SCRIPT_FILE_PATH)
         self.cam_device_connected()
 
@@ -65,6 +79,32 @@ class VideoStreamer:
             self.is_stop_pending_flag = False
             logging.info("Create new mjpg fetcher thread")
 
+    def create_new_writer(self):
+        now = strftime("%Y-%m-%d_%H:%M:%S", localtime())
+        filename = os.path.join(self.SAVE_FOLDER, f"video_{now}.avi")
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        writer = cv2.VideoWriter(filename, fourcc, int(self.current_framerate), (int(self.current_width), int(self.current_height)))
+        self.recording_start_time = time()
+        self.current_video_filename = filename
+        logging.info(f"Started new video recording: {filename}")
+        return writer
+
+    def log_video_filename(self, filename):
+        with open(self.LOG_FILE_PATH, 'a') as log_file:
+            log_file.write(os.path.basename(filename) + '\n')
+
+    def start_record(self):
+        if not self.is_recording:
+            logging.info("Starting video recording...")
+            self.video_writer = self.create_new_writer()
+            self.is_recording = True
+
+    def stop_record(self):
+        if self.is_recording:
+            logging.info("Stopping video recording...")
+            self.video_writer.release()
+            self.log_video_filename(self.current_video_filename)
+            self.is_recording = False
 
     def cam_device_disconnected(self):
         device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
@@ -97,10 +137,23 @@ class VideoStreamer:
             if a != -1 and b != -1:
                 self.mutex.acquire()
                 self.last_captured_frame = bytes[a:b+2]
+                frame = cv2.imdecode(np.frombuffer(self.last_captured_frame, dtype=np.uint8), cv2.IMREAD_COLOR)
                 self.mutex.release()
                 bytes = bytes[b+2:]
 
+                if frame is not None and self.is_recording:
+                    self.video_writer.write(frame)
+
+                if self.is_recording and time() - self.recording_start_time >= 60:
+                    self.video_writer.release()
+                    self.log_video_filename(self.current_video_filename)
+                    self.video_writer = self.create_new_writer()
+
             if self.is_stop_pending_flag:
+                if self.is_recording:
+                    self.video_writer.release()
+                    self.log_video_filename(self.current_video_filename)
+                    self.is_recording = False
                 self.is_stop_done_flag = True
                 self.is_stream_started_flag = False
                 self.is_stop_pending_flag = False
@@ -134,6 +187,9 @@ class VideoStreamer:
         else:
             logging.info("Video streamer obtained request to change stream resolution to " + resolution)
 
+        if self.is_recording:
+            self.stop_record()
+
         subprocess.call(self.STOP_STREAM_SCRIPT_FILE_PATH)
         self.request_to_stop_mjpg_fetcher()
         self.wait_stopping()
@@ -160,6 +216,9 @@ class VideoStreamer:
         self.is_stream_started_flag = True
         self.is_stop_done_flag = False
         self.is_stop_pending_flag = False
+
+        if self.is_recording:
+            self.start_record()
 
     def capture_frame(self):
         self.mutex.acquire()
